2c8e6086f3e2ec2274c3865425ac8f98d4ed2aaf
Lewis Kellett <lewisk1996@gmail.com>
2023-11-09 09:42:11 +0000

    updated

--- src/main/java/com/lewisk/testapi/Controller.java
+++ src/main/java/com/lewisk/testapi/Controller.java
@@ -1,25 +1,19 @@
 package com.lewisk.testapi;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.apache.http.client.methods.HttpPost;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.net.SocketTimeoutException;
 
 @RestController
 class ApigeeController {
@@ -27,32 +21,38 @@ class ApigeeController {
     private String makeHttpRequest(Config config)
     {
         String clientCredentials = "client_id=" + config.getClientId() + "&client_secret=" + config.getClientSecret();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .POST(HttpRequest.BodyPublishers.ofString(clientCredentials))
-                .uri(URI.create(config.getApigeeUrl()))
-                .build();
-        try {
-            long startTime = System.currentTimeMillis();
-            HttpResponse<?> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            long endTime = System.currentTimeMillis();
-            return "StatusCode: " + response.statusCode() + "\nBody: " + response.body() + "\nTime (ms): " + (endTime - startTime);
-        } catch (IOException | InterruptedException e) {
+        String result = "";
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost request = new HttpPost(config.getApigeeUrl());
+
+            // add request headers
+            request.addHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.setEntity(new org.apache.http.entity.StringEntity(clientCredentials));
+
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                // Get HttpResponse Status
+                System.out.println(response.getProtocolVersion());              // HTTP/1.1
+                System.out.println(response.getStatusLine().getStatusCode());   // 200
+                System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+                System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    result = EntityUtils.toString(entity);
+                    System.out.println(result);
+                }
+
+            }
+        } catch(SocketTimeoutException timeoutException) {
+            String msg = timeoutException.getMessage();
+            msg += "Bytes sent: " + timeoutException.bytesTransferred;
+            return msg;
+        }
+        catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @GetMapping("/api")
-    public String get() {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            String text = Files.readString(Path.of("/tmp/apiconfig.json"));
-            Config cfg = mapper.readValue(text, Config.class);
-            return makeHttpRequest(cfg);
-        } catch (Exception e) {
-            return "Error: " + e.getMessage();
-        }
+        return result;
     }
 
     @PostMapping("/api")
